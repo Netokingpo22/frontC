7b687cd8c9d34f6eccdf30f47e407c577b1b46bf
Netokingpo <ernestozb22@gmail.com>
2023-11-03 11:11:08 -0700

    Table

--- src/views/Carrera.vue
+++ src/views/Carrera.vue
@@ -1,21 +1,56 @@
 <script setup>
-import { useField, useForm } from 'vee-validate'
 import { useToast } from "vue-toastification";
 import { useRouter } from 'vue-router';
 
-const router = useRouter()
-const { handleSubmit } = useForm({
+import { ref } from 'vue'
+import { useField, useForm } from 'vee-validate'
+
+const { handleSubmit, handleReset } = useForm({
   validationSchema: {
-    username(value) {
-      if (value?.length >= 1) return true
-      return 'El nombré no puede estar vacío. '
+    name(value) {
+      if (value?.length >= 2) return true
+
+      return 'Name needs to be at least 2 characters.'
     },
-    password(value) {
-      if (value?.length >= 1) return true
-      return 'La contrseña no puede estar vacío. '
+    phone(value) {
+      if (value?.length > 9 && /[0-9-]+/.test(value)) return true
+
+      return 'Phone number needs to be at least 9 digits.'
+    },
+    email(value) {
+      if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+
+      return 'Must be a valid e-mail.'
+    },
+    select(value) {
+      if (value) return true
+
+      return 'Select an item.'
+    },
+    checkbox(value) {
+      if (value === '1') return true
+
+      return 'Must be checked.'
     },
   },
 })
+const name = useField('name')
+const phone = useField('phone')
+const email = useField('email')
+const select = useField('select')
+const checkbox = useField('checkbox')
+
+const items = ref([
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4',
+])
+
+const submit = handleSubmit(values => {
+  alert(JSON.stringify(values, null, 2))
+})
+const router = useRouter()
 const toast = useToast();
 const options = {
   position: "top-right",
@@ -31,43 +66,49 @@ const options = {
   icon: true,
   rtl: false
 };
-
-const username = useField('username')
-const password = useField('password')
-
-const logIn = handleSubmit(values => {
-  fetch('http://127.0.0.1:8000/api/v1/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(values),
-  })
-    .then(response => response.json())
-    .then(data => {
-      if (data.detail == "Not found.") {
-        toast.error("Error : \nEl usuario o la contraseña no son correctos", options);
-        return;
-      }
-      localStorage.setItem("id", data.id)
-      localStorage.setItem("username", data.username)
-      this.$router.push('home')
-    })
-    .catch((error) => {
-      toast.error("Error : \nHa ocurrido un error en el servidor.", options);
-    });
-})
 </script>
 
 <template>
   <main>
     <v-app>
-      <div class="flex">
-        <div class="bg-slate-800 h-screen w-fit">
+      <div class="flex min-h-screen">
+        <div class="bg-slate-800 w-fit">
           <NavBar></NavBar>
         </div>
-        <div class="flex justify-center items-center w-screen h-screen">
-          Carrera
+        <div class="flex flex-col w-full align-top">
+          <v-card class="w-full">
+            <div>
+              <form @submit.prevent="submit">
+                <v-text-field v-model="name.value.value" :counter="10" :error-messages="name.errorMessage.value"
+                  label="Name"></v-text-field>
+
+                <v-text-field v-model="phone.value.value" :counter="7" :error-messages="phone.errorMessage.value"
+                  label="Phone Number"></v-text-field>
+
+                <v-text-field v-model="email.value.value" :error-messages="email.errorMessage.value"
+                  label="E-mail"></v-text-field>
+
+                <v-select v-model="select.value.value" :items="items" :error-messages="select.errorMessage.value"
+                  label="Select"></v-select>
+
+                <v-checkbox v-model="checkbox.value.value" :error-messages="checkbox.errorMessage.value" value="1"
+                  label="Option" type="checkbox"></v-checkbox>
+
+                <v-btn class="me-4" type="submit">
+                  submit
+                </v-btn>
+
+                <v-btn @click="handleReset">
+                  clear
+                </v-btn>
+              </form>
+            </div>
+            <v-card-title>
+              <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line
+                hide-details></v-text-field>
+            </v-card-title>
+            <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
+          </v-card>
         </div>
       </div>
     </v-app>
@@ -76,10 +117,286 @@ const logIn = handleSubmit(values => {
 
 <script>
 import NavBar from "../components/NavBar.vue"
+import {
+  VDataTable,
+  VDataTableServer,
+  VDataTableVirtual,
+} from "vuetify/labs/VDataTable";
 
 export default {
   components: {
-    NavBar: NavBar
-  }
+    NavBar: NavBar,
+    VDataTable,
+    VDataTableServer,
+    VDataTableVirtual,
+  },
+  data() {
+    return {
+      search: '',
+      headers: [
+        {
+          align: 'start',
+          key: 'name',
+          sortable: true,
+          title: 'Dessert (100g serving)',
+        },
+        { key: 'calories', title: 'Calories' },
+        { key: 'fat', title: 'Fat (g)' },
+        { key: 'carbs', title: 'Carbs (g)' },
+        { key: 'protein', title: 'Protein (g)' },
+        { key: 'iron', title: 'Iron (%)' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: 1,
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: 1,
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: 7,
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: 8,
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: 16,
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: 0,
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: 2,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+      ],
+    }
+  },
 }
 </script>
