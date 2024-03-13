969259e035d48bd99e1601d1f73aed2e94c741ae
Netokingpo <ernestozb22@gmail.com>
2024-03-13 11:04:18 -0700

    Fix GET/PUT/SET/DELETE

--- src/Api/carreraApi.js
+++ src/Api/carreraApi.js
@@ -4,17 +4,19 @@ import toastification from '../composable/toastification'
 const { option, useToast } = toastification();
 
 const carreraApi = () => {
-    const APIURL = 'http://127.0.0.1:8000/api/v1/Carrera';
+    const APIURL = 'http://127.0.0.1:8000/api/v1/Carrera/';
 
-    function setCarrera(values) {
-        return axios.post(APIURL, values, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("token")
-            },
-        }).then(() => {
+    async function setCarrera(values) {
+        console.log(values);
+        try {
+            await axios.post(APIURL, values, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem("token")
+                },
+            });
             return true;
-        }).catch((error) => {
+        } catch (error) {
             console.log(error);
             if (error.code == "ERR_NETWORK") {
                 useToast.error("Error en el servidor", option);
@@ -22,18 +24,19 @@ const carreraApi = () => {
             }
             useToast.error("Algo salio mal", option);
             return false;
-        });
+        }
     }
 
-    function getCarreras() {
-        return axios.get(APIURL, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("token")
-            },
-        }).then(response => {
+    async function getCarreras() {
+        try {
+            const response = await axios.get(APIURL, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem("token")
+                },
+            });
             return response.data;
-        }).catch((error) => {
+        } catch (error) {
             if (error.code == "ERR_NETWORK") {
                 useToast.error("Error en el servidor", option);
                 return;
@@ -42,18 +45,19 @@ const carreraApi = () => {
                 useToast.error("El usuario ya existe", option);
                 return;
             }
-        });
+        }
     }
 
-    function editCarrera(id, values) {
-        return axios.put(`${APIURL}/${id}`, values, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("token")
-            },
-        }).then(() => {
+    async function editCarrera(id, values) {
+        try {
+            await axios.put(APIURL + id + '/', values, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem("token")
+                },
+            });
             return true;
-        }).catch((error) => {
+        } catch (error) {
             console.log(error);
             if (error.code == "ERR_NETWORK") {
                 useToast.error("Error en el servidor", option);
@@ -61,18 +65,19 @@ const carreraApi = () => {
             }
             useToast.error("Algo salio mal", option);
             return false;
-        });
+        }
     }
 
-    function deleteCarrera(id) {
-        return axios.delete(`${APIURL}/${id}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem("token")
-            },
-        }).then(() => {
+    async function deleteCarrera(id) {
+        try {
+            await axios.delete(APIURL + id + '/', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': localStorage.getItem("token")
+                },
+            });
             return true;
-        }).catch((error) => {
+        } catch (error) {
             console.log(error);
             if (error.code == "ERR_NETWORK") {
                 useToast.error("Error en el servidor", option);
@@ -80,11 +85,11 @@ const carreraApi = () => {
             }
             useToast.error("Algo salio mal", option);
             return false;
-        });
+        }
     }
-
     return {
         setCarrera, getCarreras, editCarrera, deleteCarrera
     }
 }
+
 export default carreraApi
--- src/views/Carrera.vue
+++ src/views/Carrera.vue
@@ -7,36 +7,58 @@ import SvgIcon from '@jamescoyle/vue-icon';
 import toastification from '../composable/toastification'
 import carreraApi from '../Api/carreraApi';
 import carreraValidate from '../validates/carreraValidate'
-import DialogAdd from '../components/DialogAdd.vue'
 
 const router = useRouter()
 const { option, useToast } = toastification();
-const { setCarrera, getCarreras } = carreraApi(router);
+const { setCarrera, getCarreras, editCarrera, deleteCarrera } = carreraApi(router);
 const { nombre, siglas, handleSubmit, reset } = carreraValidate();
-const dialogAdd = ref(false);
-const dialog = ref(false);
+const isAdd = ref(false);
 const isUp = ref(false);
 const isDel = ref(false);
 
 const postCarrera = handleSubmit(values => {
-  reLoad(values)
+  apiPost(values)
 })
-const editCarrera = handleSubmit(values => {
-  console.log(values);
-})
-
-const deleteCarrera = handleSubmit(values => {
-})
-
-async function reLoad(values) {
+async function apiPost(values) {
   const data = await setCarrera(values);
   if (data) {
-    dialogOpen.value = false;
+    isAdd.value = false;
     reset();
     useToast.success("Se ha agregado una nueva carrera.", option);
     const updatedCarreras = await getCarreras();
     carreras.value = updatedCarreras;
   }
+}
+
+const putCarrera = handleSubmit(values => {
+  apiPut(values)
+})
+async function apiPut(values) {
+  let newValues = { nombre: values.nombre, siglas: values.siglas };
+  const data = await editCarrera(carreraId.value, newValues);
+  if (data) {
+    isUp.value = false;
+    reset();
+    useToast.success("Se ha editado una carrera.", option);
+    const updatedCarreras = await getCarreras();
+    carreras.value = updatedCarreras;
+  }
+}
+
+async function apiDel() {
+  const data = await deleteCarrera(carreraId.value);
+  if (data) {
+    isDel.value = false;
+    reset();
+    useToast.success("Se ha elimnado una carrera.", option);
+    const updatedCarreras = await getCarreras();
+    carreras.value = updatedCarreras;
+  }
+}
+
+function addItem() {
+  reset();
+  isAdd.value = true;
 }
 
 function editeItem(param) {
@@ -49,8 +71,8 @@ function editeItem(param) {
 
 function deleteItem(param) {
   isDel.value = true;
+  carreraId.value = param.id
   nombre.value.value = param.nombre
-  console.log(param);
 }
 //Table fill ------------------------------------------
 const carreras = ref([]);
@@ -82,18 +104,34 @@ onMounted(fillTable);
     <v-app>
       <div class="flex flex-col justify-center items-center h-full bg-slate-200 text-slate-800 text-[16px] py-5">
         <div class="pt-5"></div>
-        <DialogAdd :dialogAddOpen="dialogAdd" :tittle="'Agregar carrera'">
-          <form @submit.prevent="postCarrera" class="flex flex-col justify-center items-center">
-            <v-text-field v-model="nombre.value.value" :error-messages="nombre.errorMessage.value" label="Nombre"
-              variant="outlined" class="w-full mb-2"></v-text-field>
-            <v-text-field v-model="siglas.value.value" :error-messages="siglas.errorMessage.value" label="Siglas"
-              variant="outlined" class="w-full mb-2"></v-text-field>
-            <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
-            <v-btn class="text-none w-full my-1" variant="outlined" type="submit" prepend-icon="mdi-plus">
-              <p class=" font-bold">Agregar</p>
-            </v-btn>
-          </form>
-        </DialogAdd>
+        <v-btn variant="outlined" @click="addItem()" class="max-h-[35px]" style="text-transform: none;">
+          <p class="font-bold">Agregar</p>
+        </v-btn>
+        <v-dialog v-model="isAdd" max-width="500" class="bg-black">
+          <v-card>
+            <v-card-text class="bg-slate-200 text-slate-800">
+              <div>
+                <h1 class="text-center text-2xl font-bold antialiased">Agregar carrera</h1>
+              </div>
+              <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
+              <div class="flex flex-col mt-6 mx-6 pb-2">
+                <form @submit.prevent="postCarrera" class="flex flex-col justify-center items-center">
+                  <v-text-field v-model="nombre.value.value" :error-messages="nombre.errorMessage.value" label="Nombre"
+                    variant="outlined" class="w-full mb-2"></v-text-field>
+                  <v-text-field v-model="siglas.value.value" :error-messages="siglas.errorMessage.value" label="Siglas"
+                    variant="outlined" class="w-full mb-2"></v-text-field>
+                  <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
+                  <v-btn class="text-none w-full my-1" variant="outlined" type="submit">
+                    <p class=" font-bold">Agregar</p>
+                  </v-btn>
+                  <v-btn class="text-none w-full my-1" variant="outlined" @click="isAdd = false">
+                    <p class=" font-bold">Cancelar</p>
+                  </v-btn>
+                </form>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
         <!-- Tabla---------------------------------------- -->
         <div class="flex flex-col border-solid border-2 border-slate-800 px-5 py-1 mt-3 w-3/5 h-fit">
           <div class="flex mt-1 justify-between align-middle">
@@ -121,7 +159,7 @@ onMounted(fillTable);
               <v-btn variant="outlined" color="#cca329" @click="editeItem(item)" class="max-h-[25px]">
                 <svg-icon type="mdi" :path="mdiFileEditOutline" class="text-[#cca329]  max-w-[20px]"></svg-icon>
               </v-btn>
-              <v-dialog v-model="isUp" class="w-[500px]">
+              <v-dialog v-model="isUp" max-width="500" class="bg-black">
                 <v-card>
                   <v-card-text class="bg-slate-200 text-slate-800">
                     <div>
@@ -129,14 +167,13 @@ onMounted(fillTable);
                     </div>
                     <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
                     <div class="flex flex-col mt-6 mx-6 pb-2">
-                      <form @submit.prevent="postCarrera" class="flex flex-col justify-center items-center">
+                      <form @submit.prevent="putCarrera" class="flex flex-col justify-center items-center">
                         <v-text-field v-model="nombre.value.value" :error-messages="nombre.errorMessage.value"
                           label="Nombre" variant="outlined" class="w-full mb-2"></v-text-field>
                         <v-text-field v-model="siglas.value.value" :error-messages="siglas.errorMessage.value"
                           label="Siglas" variant="outlined" class="w-full mb-2"></v-text-field>
                         <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
-                        <v-btn color="#cca329" class="text-none w-full my-1 mt-4" variant="outlined"
-                          @click="isUp = false">
+                        <v-btn color="#cca329" class="text-none w-full my-1 mt-4" variant="outlined" type="submit">
                           <p class=" font-bold">Editar</p>
                         </v-btn>
                         <v-btn class="text-none w-full my-1" variant="outlined" @click="isUp = false">
@@ -153,7 +190,7 @@ onMounted(fillTable);
               <v-btn variant="outlined" color="#cc2929" @click="deleteItem(item)" class="max-h-[25px]">
                 <svg-icon type="mdi" :path="mdiDeleteOutline" class="text-[#cc2929]  max-w-[20px]"></svg-icon>
               </v-btn>
-              <v-dialog v-model="isDel" width="auto" max-width="500">
+              <v-dialog v-model="isDel" max-width="500" class="bg-black">
                 <v-card>
                   <v-card-text class="bg-slate-200 text-slate-800">
                     <div>
@@ -161,11 +198,10 @@ onMounted(fillTable);
                     </div>
                     <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
                     <div class="flex flex-col mt-6 mx-6 pb-2">
-                      <form @submit.prevent="postCarrera" class="flex flex-col justify-center items-center">
+                      <form @submit.prevent="apiDel" class="flex flex-col justify-center items-center">
                         <h1>¿Seguro que desea eliminar "{{ nombre.value.value }}"?</h1>
                         <div class="w-full h-[2px] bg-slate-800 mt-2"></div>
-                        <v-btn color="#cc2929" class="text-none w-full my-1 mt-4" variant="outlined"
-                          @click="isDel = false">
+                        <v-btn color="#cc2929" class="text-none w-full my-1 mt-4" variant="outlined" type="submit">
                           <p class=" font-bold">Eliminar</p>
                         </v-btn>
                         <v-btn class="text-none w-full my-1" variant="outlined" @click="isDel = false">
